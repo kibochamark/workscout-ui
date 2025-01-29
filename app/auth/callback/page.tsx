ecac52c6090c86b4641ec7b5e7d587548ce8df76
kibochamark <mark.varu@student.cuk.ac.ke>
2025-01-29 16:38:32 +0300

    update on callback

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -2,13 +2,17 @@ import { Loader } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
+
     return (
         <div className='mt-18  w-full flex justify-center relative'>
 
             <div className='flex flex-col items-center gap-2'>
                 <Loader className='text-primary900 w-10 h-10 animate animate-spin text-center' />
                 <h3 className='text-displayMedium font-bold'>Redirecting....</h3>
-                <p>Please wait....</p>
+                <p>Please wait....
+
+                    click on the dashboard icon to proceed
+                </p>
             </div>
         </div>
     )
