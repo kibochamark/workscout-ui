293034eea938e57b5b36c8f3a0ae46cfdcbdf1d7
Mark kibocha <regexmark1@gmail.com>
2025-04-24 17:36:59 +0300

    Update route.js

--- app/api/stripe/customer-portal/route.js
+++ app/api/stripe/customer-portal/route.js
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
 
 
 export async function POST(request) {
