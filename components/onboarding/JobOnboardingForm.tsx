87e1cc2d02f299732591a5d876d0cd68e03544b9
GidyMane <muragegideon2000@gmail.com>
2025-06-05 15:17:17 +0300

    update

--- components/onboarding/JobOnboardingForm.tsx
+++ components/onboarding/JobOnboardingForm.tsx
@@ -171,7 +171,7 @@ console.log(user, "user");
                   {step === 1 ? (
                     <div className="space-y-6">
                       <div>
-                        <p className="text-sm text-muted-foreground">Let us get started</p>
+                        {/* <p className="text-sm text-muted-foreground">Let us get started</p> */}
                         <h1 className="text-2xl font-semibold mt-1">What is your job description?</h1>
                       </div>
 
